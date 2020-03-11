61858f02a9c98c43364ff340a28cb2829f639375
Dung-Yi <dungyichao@gmail.com>
2020-03-11 14:56:47 -0400

    Add files via upload

---
+++ BSP/osKernel.h
@@ -0,0 +1,24 @@
+#ifndef _OS_KERNEL_H
+#define _OS_KERNEL_H
+#include "stm32f4xx.h"                  // Device header
+#include <stdint.h>
+
+void osKernelLaunch(uint32_t quanta);
+
+void osKernelInit(void);
+
+uint8_t osKernelAddThreads(void(*task0)(void),
+													 void(*task1)(void),
+													 void(*task2)(void));
+
+void osSemaphoreInit(int32_t *semaphore, int32_t value);
+
+void osThreadYield(void);
+void osSignalSet(int32_t *semaphore);
+void osSignalWait(int32_t *semaphore);													 
+#define PERIOD		100
+//void Task3(void);
+
+#endif
+													 
+													 
