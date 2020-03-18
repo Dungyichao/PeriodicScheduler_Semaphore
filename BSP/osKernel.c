e68d31cb79fe5eb58789dbfd5eb79473953791ac
Dung-Yi <dungyichao@gmail.com>
2020-03-18 11:24:49 -0400

    Update osKernel.c

--- BSP/osKernel.c
+++ BSP/osKernel.c
@@ -31,27 +31,28 @@ void osKernelStackInit(int i){
 
 
 uint8_t osKernelAddThreads(void(*task0)(void),void(*task1)(void),void(*task2)(void))
-	{ 
+{ 
 	__disable_irq();
   tcbs[0].nextPt = &tcbs[1]; 
   tcbs[1].nextPt = &tcbs[2]; 
   tcbs[2].nextPt = &tcbs[0]; 
   osKernelStackInit(0);
-	TCB_STACK[0][STACKSIZE-2] = (int32_t)(task0); 
+  TCB_STACK[0][STACKSIZE-2] = (int32_t)(task0); 
   
-	osKernelStackInit(1);
-	TCB_STACK[1][STACKSIZE-2] = (int32_t)(task1); 
+  osKernelStackInit(1);
+  TCB_STACK[1][STACKSIZE-2] = (int32_t)(task1); 
   
-	osKernelStackInit(2);
-	TCB_STACK[2][STACKSIZE-2] = (int32_t)(task2); 
-	currentPt = &tcbs[0];
- __enable_irq();
-		return 1;              
+  osKernelStackInit(2);
+  TCB_STACK[2][STACKSIZE-2] = (int32_t)(task2); 
+  currentPt = &tcbs[0];
+  __enable_irq();
+	
+  return 1;              
 }
 void osKernelInit(void)
 {
      __disable_irq();
-		 MILLIS_PRESCALER=(BUS_FREQ/1000);
+     MILLIS_PRESCALER=(BUS_FREQ/1000);
 
 }
 void osKernelLaunch(uint32_t quanta)
@@ -59,7 +60,7 @@ void osKernelLaunch(uint32_t quanta)
 	SysTick->CTRL =0;
 	SysTick->VAL=0;
 	SysTick->LOAD = (quanta* MILLIS_PRESCALER)-1;
-  //SYSPRI3 =(SYSPRI3&0x00FFFFFF)|0xE0000000; // priority 7
+        //SYSPRI3 =(SYSPRI3&0x00FFFFFF)|0x70000000; // priority 7
 	NVIC_SetPriority(SysTick_IRQn, 0x00000000);
 
 	SysTick->CTRL =0x00000007;
@@ -71,7 +72,7 @@ void osKernelLaunch(uint32_t quanta)
 void osThreadYield(void)
 { 
 	SysTick->VAL = 0;
-  ICSR = 0x04000000; // trigger SysTick
+        ICSR = 0x04000000; // trigger SysTick
 }
 
 uint32_t period_tick;
@@ -102,9 +103,9 @@ void osSignalWait(volatile int32_t *semaphore)
 	//__disable_irq();
 	while(*semaphore <=0)
 	{		
-			__disable_irq();
-			//osThreadYield();
-			__enable_irq();
+		__disable_irq();
+		//osThreadYield();  // Non-preemptive method
+		__enable_irq();
 	}
 	*semaphore -= 0x01;
 	__enable_irq();
